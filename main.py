e660f502546002776966875ce0b155782b91a78e
Ben alaluf <94129183+benalaluf@users.noreply.github.com>
2023-01-18 11:00:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+__author__ = 'Ben'
+
+import pygame
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+    pygame.display.set_caption("Ben's Game")
+    clock = pygame.time.Clock()
+
+    test_surface = pygame.Surface((100, 200))
+    test_surface.fill('Blue')
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        screen.blit(test_surface, (0, 0))
+        pygame.display.update()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    print(r"""    
+          __          ___         
+         / /  __ __  / _ )___ ___ 
+        / _ \/ // / / _  / -_) _ \
+       /_.__/\_, (_)____/\__/_//_/
+            /___/                      
+                         """)
+    main()
